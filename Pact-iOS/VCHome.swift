f53935224285970f14be92869eabc351956636b4
Matt Gu <matthewgu93@gmail.com>
2017-04-28 18:20:25 +0800

    Pull to refresh

---
+++ Pact-iOS/VCHome.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Pact-iOS
+//
+//  Created by matt on 2017-04-28.
+//  Copyright © 2017 matt. All rights reserved.
+//
+
+import UIKit
+
+class VCHome: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(r: 61, g: 91, b: 151, a: 1)
+        
+        view.addSubview(scrlv)
+        
+        setupScrlv()
+        
+        // refresh control
+        let refreshControl = UIRefreshControl()
+        let title = NSLocalizedString("Pull To Refresh", comment: "Pull to refresh")
+        refreshControl.attributedTitle = NSAttributedString(string: title)
+        refreshControl.addTarget(self,
+                                 action: #selector(refreshOptions(sender:)),
+                                 for: .valueChanged)
+        scrlv.refreshControl = refreshControl
+    }
+
+    let scrlv: UIScrollView = {
+        let scrlv = UIScrollView()
+        scrlv.backgroundColor = UIColor.white
+        scrlv.translatesAutoresizingMaskIntoConstraints = false
+        return scrlv
+    }()
+    
+    let contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.gray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    func setupScrlv() {
+        scrlv.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        scrlv.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        scrlv.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        scrlv.topAnchor.constraint(equalTo: view.topAnchor, constant: 64).isActive = true
+        
+        // set up content view
+        scrlv.addSubview(contentView)
+        contentView.leftAnchor.constraint(equalTo: scrlv.leftAnchor).isActive = true
+        contentView.rightAnchor.constraint(equalTo: scrlv.rightAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: scrlv.bottomAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: scrlv.topAnchor).isActive = true
+        contentView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        contentView.heightAnchor.constraint(equalTo: view.heightAnchor, constant: -64).isActive = true
+    }
+    
+    @objc private func refreshOptions(sender: UIRefreshControl) {
+        sender.endRefreshing()
+        print("refresh working!")
+    }
+
+}
+
+extension UIColor
+{
+    
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat  ) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: a)
+    }
+    
+}
