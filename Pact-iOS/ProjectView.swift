20c96a9d30a866a7c34090113a663403f534be9f
Matt Gu <matthewgu93@gmail.com>
2017-04-29 13:39:10 +0800

    Finished custom view xib

---
+++ Pact-iOS/ProjectView.swift
@@ -0,0 +1,21 @@
+//
+//  ProjectView.swift
+//  Pact-iOS
+//
+//  Created by matt on 2017-04-29.
+//  Copyright © 2017 matt. All rights reserved.
+//
+
+import UIKit
+
+class ProjectView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
