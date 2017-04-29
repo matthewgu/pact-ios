344ac47ea2fc477e4d10284050217a07e50918d5
Matt Gu <matthewgu93@gmail.com>
2017-04-29 12:20:54 +0800

    Fetching and displaying user points from firebase db!

--- Pact-iOS/VCHome.swift
+++ Pact-iOS/VCHome.swift
@@ -176,27 +176,32 @@ class VCHome: UIViewController {
         getStep()
     }
     
-    @IBAction func logoutBtnPressed(_ sender: Any) {
-        handleLogout()
-    }
-    
-    // MARK: - Support
     func checkIfUserIsLoggedIn() {
         if FIRAuth.auth()?.currentUser?.uid == nil {
             print("user not signed in")
             perform(#selector(handleLogout), with: nil, afterDelay: 0)
         } else {
-            let uid = FIRAuth.auth()?.currentUser?.uid
-            FIRDatabase.database().reference().child("users:").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
-                
-                if let dictionary = snapshot.value as? [String: AnyObject] {
-                    print(dictionary["name"]!)
-                }
-                
-            }, withCancel: nil)
+            fetchPoints()
         }
-
+        
     }
+    
+    func fetchPoints() {
+        let uid = FIRAuth.auth()?.currentUser?.uid
+        FIRDatabase.database().reference().child("users:").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            if let dictionary = snapshot.value as? [String: AnyObject] {
+                self.pointsLabel.text = dictionary["points"] as? String
+            }
+            
+        }, withCancel: nil)
+    }
+    
+    @IBAction func logoutBtnPressed(_ sender: Any) {
+        handleLogout()
+    }
+    
+    // MARK: - Support
     
     func handleLogout() {
         do {
